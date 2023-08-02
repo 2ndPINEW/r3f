8705fa077f058b166123ef8237d10dc0560e2b6c
Eito Takase <eito01178@gmail.com>
2023-08-02 18:56:56 +0900

    いろいろ

--- src/app/playground/page.tsx
+++ src/app/playground/page.tsx
@@ -1,20 +1,21 @@
 import { WebGLCompatibility } from "@/app/_core/WebGLCompatibility/WebGLCompatibility";
 import { RotateBox } from "./_components/001-hello-world/rotateBox";
 import { DrawLine } from "./_components/001-hello-world/drawLine";
+import { HelloR3f } from "./_components/001-hello-world/helloR3f";
 
 export default function Page() {
   return (
     <main>
-      <WebGLCompatibility
-        loading={<div>loading</div>}
-        fallback={<div>fallback</div>}
-      >
-        <div>RotateBox</div>
+      <WebGLCompatibility loading={<div>Checking...</div>}>
+        <h2>RotateBox</h2>
         <RotateBox />
-        <div>DrawLine</div>
+
+        <h2>HelloR3f</h2>
+        <HelloR3f></HelloR3f>
+
+        <h2>DrawLine</h2>
         <DrawLine />
       </WebGLCompatibility>
-      <p>heloo</p>
     </main>
   );
 }
